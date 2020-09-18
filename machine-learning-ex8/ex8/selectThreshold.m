9e1c3d9bd0a4fdf817260333e0cbcdf93e1e949e
Roman <kov2novych@gmail.com>
2020-09-18 16:17:34 -0400

    Exercise 8

---
+++ machine-learning-ex8/ex8/selectThreshold.m
@@ -0,0 +1,56 @@
+function [bestEpsilon bestF1] = selectThreshold(yval, pval)
+%SELECTTHRESHOLD Find the best threshold (epsilon) to use for selecting
+%outliers
+%   [bestEpsilon bestF1] = SELECTTHRESHOLD(yval, pval) finds the best
+%   threshold to use for selecting outliers based on the results from a
+%   validation set (pval) and the ground truth (yval).
+%
+
+bestEpsilon = 0;
+bestF1 = 0;
+F1 = 0;
+
+stepsize = (max(pval) - min(pval)) / 1000;
+for epsilon = min(pval):stepsize:max(pval)
+    
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Compute the F1 score of choosing epsilon as the
+    %               threshold and place the value in F1. The code at the
+    %               end of the loop will compare the F1 score for this
+    %               choice of epsilon and set it to be the best epsilon if
+    %               it is better than the current choice of epsilon.
+    %               
+    % Note: You can use predictions = (pval < epsilon) to get a binary vector
+    %       of 0's and 1's of the outlier predictions
+	prediction = (pval < epsilon);
+	tp = sum((prediction == 1)&(yval==1));
+	fp = sum((prediction == 1)&(yval==0));
+	fn = sum((prediction == 0)&(yval==1));
+	F1 = 0;
+
+	% for i = 1:size(prediction,1)
+	% 	if yval(i)==1 && prediction(i)==1
+	% 		++tp;
+	% 	endif
+	% 	if yval(i)==0 && prediction(i)==1
+	% 		++fp;
+	% 	endif
+	% 	if yval(i)==1 && prediction(i)==0
+	% 		++fn;
+	% 	endif
+	% endfor
+
+	if tp > 0
+		prec = tp / (tp + fp);
+		rec = tp/(tp+fn);
+		F1 = (2 * prec * rec)/(prec + rec);
+	endif
+    % =============================================================
+
+    if F1 > bestF1
+       bestF1 = F1;
+       bestEpsilon = epsilon;
+    end
+end
+
+end
